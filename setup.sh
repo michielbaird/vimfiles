d45fa108df692b0c6944b1bf4331cf21b96e6751
Graham Poulter <graham@mocality.com>
2012-03-26 09:41:04 +0200

    Improve setup.sh to do relative links in home directory

--- setup.sh
+++ setup.sh
@@ -1,49 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+
+ADIR="$(dirname $(readlink -f $BASH_SOURCE))"
+RDIR="${ADIR##$HOME/}"
+echo $ADIR
+echo $RDIR
 
 set -e
 set -u
 
-mydir=`dirname $0`
-cd ${mydir}
-mydir=`pwd`
+cd "$ADIR"
 
-if [ ! -e pathogen ]; then
+if [[ ! -e pathogen ]]; then
     echo "You do not have the git submodules.  Will try do it for you..."
-    failed=0
-    git submodule update --init || failed=1
-    if [ $failed -eq 1 ]; then
-	echo "Could not update the modules for you.  You will need to do it yourself."
-	echo ""
-	echo "    git submodule update --init"
-	exit 1
-    fi
-fi
-
-if [ -e ~/.vim ]; then
-    if [ ! -L ~/.vim ]; then
-        echo "You have a ~/.vim that isn't a symlink.  It will not be deleted.  Please take care of it."
+    git submodule update --init
+    if [[ $? -ne 0  ]]; then
+        echo "Could not update the modules for you.  You will need to do it yourself."
+        echo
+        echo "    git submodule update --init"
         exit 1
     fi
 fi
 
-
-if [ -e ~/.vimrc ]; then
-    if [ ! -L ~/.vimrc ]; then
-        echo "You have a ~/.vimrc that isn't a symlink.  It will not be deleted.  Please take care of it."
+for vimlink in ~/.vim ~/.vimrc ~/.gvimrc; do
+    if [[ -e "$vimlink" ]] && [[ ! -L "$vimlink" ]]; then
+        echo "You have a $vimlink that isn't a symlink.  It will not be deleted.  Please take care of it."
         exit 1
     fi
-fi
+done
 
-if [ -e ~/.gvimrc ]; then
-    if [ ! -L ~/.gvimrc ]; then
-        echo "You have a ~/.gvimrc that isn't a symlink.  It will not be deleted.  Please take care of it."
-        exit 1
-    fi
-fi
-
-rm -f ~/.vim
-rm -f ~/.vimrc
-rm -f ~/.gvimrc
-ln -s ${mydir} ~/.vim
-ln -s ${mydir}/vimrc ~/.vimrc
-ln -s ${mydir}/gvimrc ~/.gvimrc
+rm -fv ~/.vim
+rm -fv ~/.vimrc
+rm -fv ~/.gvimrc
+ln -sv "$RDIR" ~/.vim
+ln -sv "$RDIR/vimrc" ~/.vimrc
+ln -sv "$RDIR/gvimrc" ~/.gvimrc
